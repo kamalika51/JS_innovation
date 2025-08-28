88b5cbe5286cb390b5b53acf65f45afa1cf0d683
kamalika51 <bkamalika49@gmail.com>
2025-08-28 07:40:41 +0530

    Script.js

--- Day1/Project/Script.js
+++ Day1/Project/Script.js
@@ -1,1 +1,33 @@
 
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result = "Please enter valid numbers.";
+    } else {
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 === 0) {
+                    result = "Cannot divide by zero.";
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                result = "Select a valid operator.";
+        }
+    }
+    document.getElementById('result').textContent = "Result: " + result;
+}
